82a588f53ce89808f669a5ccff007f4877c83da1
Dylan <dylanyeowf@gmail.com>
2025-07-11 10:56:19 +0800

    removed unused logging

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ async function loadKnownFaces() {
 
     const res = await fetch(file);
     const data = await res.json();
-    console.log(data);
 
     knownFaces = data.map(user => ({
         name: user.name,
@@ -32,7 +31,6 @@ function findBestMatch(descriptor) {
     let best = { name: "Unknown", distance: 1.0 }; // Set the initial best match to Unknown with max possible distance (1.0)
     for (const user of knownFaces) {
         const dist = faceapi.euclideanDistance(descriptor, user.descriptor);
-        console.log(dist);
         if (dist < best.distance && dist <= 0.3) {
             best = { name: user.name, distance: dist };
         }
