f95979f3a61175c571f4e4a9be3830319f7546e5
Dylan <dylanyeowf@gmail.com>
2025-07-19 10:58:11 +0800

    added stats

--- main.js
+++ main.js
@@ -28,10 +28,17 @@ let challengePassed = false;
 
 // Function to find the best match for the detected face descriptor
 function findBestMatch(descriptor) {
+    let targetElement = document.getElementById("check");
+    targetElement.innerHTML = ""; // Clear previous results
     let best = { name: "Unknown", distance: 1.0 }; // Set the initial best match to Unknown with max possible distance (1.0)
     for (const user of knownFaces) {
         const dist = faceapi.euclideanDistance(descriptor, user.descriptor);
-        document.getElementById("check").textContent = `${user.name} (${(1 - dist)*100})`
+        
+        let new1 = document.createElement("p");
+        new1.textContent = `${user.name} (${(1 - dist) * 100})`;
+        
+        targetElement.insertBefore(new1, targetElement.firstChild);
+
         if (dist < best.distance && dist <= 0.3) {
             best = { name: user.name, distance: dist };
         }
